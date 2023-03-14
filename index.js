773a035e8d9a75d9f6a369a8ddda24a133cf92a3
Jason Stewart <jaysin.stewart@gmail.com>
2023-03-14 09:58:28 -0700

    Completed control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance){
+  if (rideDistance <= 400) {
+    return "This one is on me!"
+  } else if (rideDistance >= 400 && rideDistance <= 2000) {
+    return "That will be twenty bucks."
+  } else if (rideDistance >= 2000 && rideDistance <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (rideDistance >= 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  const result = (city === "NYC") ? "Ok, sounds good." : "No go.";
+  return result;
+} 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let tipReply; 
+
+  switch (tip) {
+    case 'generous':
+      return tipReply = 'Thank you so much.';
+    case 'not as generous':
+      return tipReply = 'Thank you.';
+    default:
+      return "Bye.";
+  }
 }
